f72d5e0fdb1842d34e1576614d346967222a80b7
Grigor Baltadjian <gb795@hotmail.com>
2021-04-03 00:55:27 +0000

    findKey

---
+++ findKey.js
@@ -0,0 +1,84 @@
+const findKey = function(array, callback) {
+  for (let key in array) {
+    let data = array[key]
+    let returnValue = callback(data)
+    
+    
+    if (returnValue === true) {
+     console.log(key)
+      return key
+    } 
+  } 
+  return undefined
+}
+
+ 
+
+    
+  
+ 
+
+
+
+
+const assertEqual = function(actual, expected) {
+  let happyEmoji = String.fromCodePoint(0x1F91D);
+  let angryEmoji = String.fromCodePoint(0x1F621);
+  if (actual === expected) {
+    console.log(`${happyEmoji} Assertion Passed: [${actual}] === [${expected}]`);
+  } else console.log(`${angryEmoji} Assertion Failed: [${actual}] !== [${expected}]`);
+};
+
+
+
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3), "Akaleri")
+
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 5) 
+
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma")
+
+/*const findKey = function(array, callback) {
+  for (let key in array) {
+    let data = array[key]
+    //console.log(data) 
+    if (callback(data)) {
+     console.log(key)
+     break
+    } 
+      
+    
+    
+  } 
+ 
+}*/
