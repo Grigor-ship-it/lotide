2012415f251a4dd4e68a0d7dd3def00f4111666d
Grigor Baltadjian <gb795@hotmail.com>
2021-04-05 16:40:49 +0000

    assertEqualV2

--- assertEqual.js
+++ assertEqual.js
@@ -2,11 +2,8 @@ const assertEqual = function(actual, expected) {
   let happyEmoji = String.fromCodePoint(0x1F91D);
   let angryEmoji = String.fromCodePoint(0x1F621);
   if (actual === expected) {
-    console.log(happyEmoji + " Assertion Passed: [" + actual + "] === [" + expected + "]");
-  } else console.log(angryEmoji + " Assertion Failed: [" + actual + "] !== [" + expected + "]");
+    console.log(`${happyEmoji} Assertion Passed: [${actual}] === [${expected}]`);
+  } else console.log(`${angryEmoji} Assertion Failed: [${actual}] !== [${expected}]`);
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(2,5);
+module.exports = assertEqual;
