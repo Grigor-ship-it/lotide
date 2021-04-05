95a8b6f3edb07d5220fe1b2238b586884c2fa1d8
Grigor Baltadjian <gb795@hotmail.com>
2021-04-05 21:06:45 +0000

    assertArraysEqualFinal

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,24 +1,4 @@
-const assertEqual = function(actual, expected) {
-  let happyEmoji = String.fromCodePoint(0x1F91D);
-  let angryEmoji = String.fromCodePoint(0x1F621);
-  if (actual === expected) {
-    console.log(`${happyEmoji} Assertion Passed: [${actual}] === [${expected}]`);
-  } else console.log(`${angryEmoji} Assertion Failed: [${actual}] !== [${expected}]`);
-};
-
-const eqArrays = function(firstArray, secondArray) {
-  if (firstArray.length !== secondArray.length) {
-  return false
- } 
-  for (let x = 0; x < firstArray.length; x++) {
-    if (firstArray[x] !== secondArray[x]) {
-       return false
-     
-      
-   } 
- } 
- return true
-}
+const eqArrays = require('../lotide/eqArrays');
 
 const assertArraysEqual = function(actual, expected) {
   let happyEmoji = String.fromCodePoint(0x1F91D);
@@ -29,10 +9,7 @@ const assertArraysEqual = function(actual, expected) {
     console.log(`${angryEmoji} Assertion Failed: [${actual}] !== [${expected}]`);
 }};
 
+module.exports = assertArraysEqual;
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);
-assertArraysEqual([1, 2, 3, 5], [1, 2, 3, 8]);
-assertArraysEqual([1, 2, 3, 5], [1, 2, 3,]);
-assertArraysEqual([1, 2, 3, 5], [1, 2, 3, 5]);
+
 
