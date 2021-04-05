a1ad04f868afc2326f5a5417a26873f27d3330f0
Grigor Baltadjian <gb795@hotmail.com>
2021-04-05 18:03:18 +0000

    tailV2

--- tail.js
+++ tail.js
@@ -1,28 +1,10 @@
-const assertEqual = function(actual, expected) {
-  let happyEmoji = String.fromCodePoint(0x1F91D);
-  let angryEmoji = String.fromCodePoint(0x1F621);
-  if (actual.length === expected.length) {
-    for (let x = 0; x < actual.length; x++) {
-      if (actual[x] === expected[x]) {
-    
-  
-    console.log(`${happyEmoji} Assertion Passed: [${actual}] === [${expected}]`);
-  } else console.log(`${angryEmoji} Assertion Failed: [${actual}] !== [${expected}]`);
-  }}};
-
 const tail = function(actual) {
   let newArray = [];
   for (let x = 1; x < actual.length; x++) {
-    newArray.push(actual[x])
+    newArray.push(actual[x]);
     
-  } 
+  }
   return newArray;
-}
+};
 
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]);
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); 
-assertEqual(words.length, 3); 
+module.exports = tail;
